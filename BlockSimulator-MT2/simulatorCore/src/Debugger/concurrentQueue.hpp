c4be0b9268e99f3de351c14698b51847ae0780e7
Andre Naz <Andre.Naz@ensimag.grenoble-inp.fr>
2013-09-09 11:51:21 -0400

    ConcurrentQueue

--- BlockSimulator-MT2/simulatorCore/src/Debugger/concurrentQueue.hpp
+++ BlockSimulator-MT2/simulatorCore/src/Debugger/concurrentQueue.hpp
@@ -1,93 +1,48 @@
-/* Multiple producers, one consumer
- * 
- * Source:
- * http://www.justsoftwaresolutions.co.uk/threading/implementing-a-thread-safe-queue-using-condition-variables.html
- */
+/* ConcurrentQueue : Multiple producers, one consumer */
+
 #include <queue>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/condition.hpp>
 
-template<typename Data>
-class ConcurrentQueue
-{
+template<typename Elem>
+
+class ConcurrentQueue {
+
 private:
-    std::queue<Data> the_queue;
-    mutable boost::mutex the_mutex;
-    boost::condition_variable the_condition_variable;
+   std::queue<Elem> queue;
+   mutable boost::mutex m;
+   boost::condition_variable cond;
+
 public:
-    void push(Data const& data)
-    {
-        boost::mutex::scoped_lock lock(the_mutex);
-        the_queue.push(data);
-        lock.unlock();
-        the_condition_variable.notify_all();
-    }
-
-    bool empty() const
-    {
-        boost::mutex::scoped_lock lock(the_mutex);
-        return the_queue.empty();
-    }
-
-    bool try_pop(Data& popped_value)
-    {
-        boost::mutex::scoped_lock lock(the_mutex);
-        if(the_queue.empty())
-        {
-            return false;
-        }
-        
-        popped_value=the_queue.front();
-        the_queue.pop();
-        return true;
-    }
-
-	void wait() {
-		boost::mutex::scoped_lock lock(the_mutex);
-        while(the_queue.empty())
-        {
-            the_condition_variable.wait(lock);
-        }
-	}
-		    
-	void timed_wait() {
-		boost::system_time const timeout=boost::get_system_time() + boost::posix_time::seconds(1);
-
-		boost::mutex::scoped_lock lock(the_mutex);
-		if(the_queue.empty())
-		{
-          the_condition_variable.timed_wait(lock,timeout);
-		}
-	}
-
-    void wait_and_pop(Data& popped_value)
-    {
-        boost::mutex::scoped_lock lock(the_mutex);
-        while(the_queue.empty())
-        {
-            the_condition_variable.wait(lock);
-        }
-        
-        popped_value=the_queue.front();
-        the_queue.pop();
-    }
     
-    Data& front()
-    {
-        boost::mutex::scoped_lock lock(the_mutex);
-        return the_queue.front();
-    }
-    
-    Data const& front() const
-    {
-        boost::mutex::scoped_lock lock(the_mutex);
-        return the_queue.front();
-    }
+   void push(Elem const& e) {
+      boost::mutex::scoped_lock lock(m);
+      queue.push(e);
+      lock.unlock();
+      cond.notify_all();
+   }
 
-    void pop()
-    {
-        boost::mutex::scoped_lock lock(the_mutex);
-        the_queue.pop();
-    }
+   bool empty() const {
+      boost::mutex::scoped_lock lock(m);
+      return queue.empty();
+   }
+
+   void timed_wait() {
+      boost::system_time const timeout = boost::get_system_time() + boost::posix_time::seconds(1);
+      boost::mutex::scoped_lock lock(m);
+      if(queue.empty()) {
+         cond.timed_wait(lock,timeout);
+      }
+   }
+    
+   Elem& front() {
+      boost::mutex::scoped_lock lock(m);
+      return queue.front();
+   }
+
+   void pop() {
+      boost::mutex::scoped_lock lock(m);
+      queue.pop();
+   }
 
 };
